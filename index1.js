122fc006cadc4b74276c252912d835ddc73876b2
hulu12 <runhigher2010@163.com>
2015-01-20 19:44:09 +0800

    day5 blog project

--- index1.js
+++ index1.js
@@ -4,14 +4,36 @@
  */
 var http = require('http');
 var assert=require('assert');
+var fs=require('fs');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+
+}).listen(1337, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:1337/');
+x = 5;
+setTimeout(function () {
+  debugger;
+  console.log("world");
+}, 1000);
+console.log(process.argv[2]);
+console.log(Number(process.argv[3]));
+
+module.exports.hello=function(){
+	console.log('hello pang 123');
+};
+
+var i = 0;
+
+function count() {
+    return ++i;
+}
+
+module.exports.count = count();
 
 
-http.createServer(function (request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello Pang\n');
-}).listen(8124);
-
-console.log('Server running at http://127.0.0.1:8124/');
+/*
 console.log(__filename);
 console.log(__dirname);
 
@@ -25,3 +47,4 @@ setTimeout(function(err, d) {
 },3000);
 
 console.log('hello pang ye xing');
+*/
---
+++ index2.js
@@ -0,0 +1,10 @@
+var vars=require('./index1');
+vars.hello();
+console.log(vars.count);
+var bin = new Buffer([ 0x68, 0x65, 0x6c, 0x6c, 0x6f ]);
+var dup = new Buffer(bin.length);
+
+bin.copy(dup);
+dup[0] = 0x48;
+console.log(bin); // => <Buffer 68 65 6c 6c 6f>
+console.log(dup); // => <Buffer 48 65 65 6c 6f>
